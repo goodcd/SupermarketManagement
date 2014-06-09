e7feb47403d1c26d879e3dcd2f591e8aacb8ded2
MiracleWong <cfwr1991@126.com>
2014-06-09 10:26:07 +0800

    在工程中增加商品销售查询的find_Sales类

---
+++ SupermarketManagement/src/com/cs/from/find_Sales.java
@@ -0,0 +1,32 @@
+package com.cs.from;
+
+import java.awt.Frame;
+
+import javax.swing.JDialog;
+
+public class find_Sales extends JDialog {
+
+	public find_Sales(Frame ower, String title, boolean modal) {
+		super(ower, title, modal);
+		try {
+			//在Java窗体正常关闭时调用，同时将整个程序从内存中清除。
+			setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+			jbInit();		//窗口的初始化函数
+			pack();			//刷新窗口
+		} catch (Exception e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		}
+	}
+	public find_Sales() {
+		// TODO Auto-generated constructor stub
+		this(new Frame(), "销售明细检查", false);
+		this.setSize(600, 620);
+		this.setLocation(350, 50);
+		this.setVisible(true);
+	}
+	
+	public void jbInit() {
+
+	}
+}
