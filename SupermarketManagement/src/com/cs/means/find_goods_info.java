010a35a0e52aec233fe6e7d836303385e8f96046
MiracleWong <cfwr1991@126.com>
2014-06-15 20:44:15 +0800

    增加数据库的操作类find_goods_info，功能：寻在商品的信息，具体内容未实现

---
+++ SupermarketManagement/src/com/cs/means/find_goods_info.java
@@ -0,0 +1,15 @@
+package com.cs.means;
+
+import java.util.Vector;
+import java.sql.*;
+
+public class find_goods_info {
+	
+	private static final String select1 =
+			"select * from Merchandise where SortNumber=?";
+	//¹¹Ôìº¯Êý
+	public find_goods_info() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
