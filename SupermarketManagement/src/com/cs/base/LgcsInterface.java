6cf890318eaa855d55631e9c1de8817e665b7d12
MiracleWong <cfwr1991@126.com>
2014-06-21 20:30:42 +0800

    增加Interface接口——LgcsInterface，利用Vector可变长的特性来储存信息

---
+++ SupermarketManagement/src/com/cs/base/LgcsInterface.java
@@ -0,0 +1,11 @@
+package com.cs.base;
+
+import java.util.Vector;
+
+public interface LgcsInterface {
+	public Vector select_merch_Name(String merch_Name);
+    public Vector select_merch_number(String merch_number);
+    public Vector select_merch_supply(String merch_supply);
+    public Vector select_merch_producing(String merch_producing);
+    public Vector select_sort_producing(String merch_sort);
+}
